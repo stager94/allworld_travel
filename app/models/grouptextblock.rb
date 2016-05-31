3553400f84cd7f589af7078d18e4a64fffbb4a9b
Stanislav Gernyak <stager94@mail.ru>
2016-05-31 15:14:53 +0300

    fix admin issue

--- app/models/grouptextblock.rb
+++ app/models/grouptextblock.rb
@@ -7,7 +7,7 @@ class Grouptextblock < ActiveRecord::Base
 
   attr_accessible :icon
 
-  before_save :destroy_image?
+  # before_save :destroy_image?
   
   rails_admin do
     navigation_label I18n.t('menu.setting')
