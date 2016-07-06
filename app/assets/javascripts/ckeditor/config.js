b3967f956ed3c0a85124416ed9db53c979f538d1
Stanislav Gernyak <stager94@mail.ru>
2016-07-07 01:15:38 +0300

    add 13 font size to ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -32,6 +32,7 @@ CKEDITOR.editorConfig = function( config )
   config.filebrowserUploadUrl = "/ckeditor/attachment_files";
 
   config.allowedContent = true;
+  config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;13/13px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;36/36px;48/48px;72/72px;';
 
   // Rails CSRF token
   config.filebrowserParams = function(){
