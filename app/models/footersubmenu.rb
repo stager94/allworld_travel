abfd20468f7ced54f3c164e254c5e769e8cb300b
Stanislav Gernyak <stager94@mail.ru>
2016-07-11 21:34:32 +0300

    fix footer styles

--- app/models/footersubmenu.rb
+++ app/models/footersubmenu.rb
@@ -3,7 +3,7 @@ class Footersubmenu < ActiveRecord::Base
   attr_accessible :name, :sortorder, :url, :footermenu_id, :icon
 
   attr_accessible :icon
-  has_attached_file :icon, :styles => { micro: "50x50#" }, default_url: ""
+  has_attached_file :icon, styles: { micro: "50x50>" }, default_url: ""
   validates_attachment_content_type :icon, :content_type => /\Aimage\/.*\Z/
 
   rails_admin do
