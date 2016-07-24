1193ea5d7979c482bdc59ec7fb1fc1fb5d4e41a3
Stanislav Gernyak <stager94@mail.ru>
2016-07-24 18:43:24 +0300

    setting up sending emails via mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ Allworld::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+    address:   'smtp.mandrillapp.com',
+    port:      587,
+    user_name: 'allworld-travel',
+    password:  'cCi5eSNtJNFpsLNRH0eq'
+  }
+
+  config.action_mailer.default_url_options = { host: 'allworld-travel.com' }
 end
